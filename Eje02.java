0f31c282fcb9b7c345560852ec8310424f97ca21
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-10-23 09:50:26 +0200

    commit

---
+++ Eje02.java
@@ -0,0 +1,14 @@
+/*Ejercicio 2
+Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while.
+*/
+
+public class Eje02 {
+    public static void main(String[] args) {
+        int i=0;
+
+        while(i<=100){
+            System.out.print(i+" ");
+            i+=5;
+        }
+    }
+}
