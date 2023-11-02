16578279aa08c56567f278b3d5e86621ce3b21bd
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-11-02 08:21:37 +0100

    commit

---
+++ Eje41.java
@@ -0,0 +1,32 @@
+/*Ejercicio 41
+Escribe un programa que diga cuántos dígitos pares y cuántos dígitos impares
+hay dentro de un número. Se recomienda usar long en lugar de int ya que el
+primero admite números más largos.
+*/
+
+public class Eje41 {
+    public static void main(String[] args) {
+
+        System.out.print("Por favor, introduzca un número entero positivo: ");
+        long n = Long.parseLong(System.console().readLine());
+        long guarda = n;
+
+        int par=0;
+        int imp=0;
+
+        while(n>0){
+            int cifra = (int)n%10;
+
+            if(cifra%2==0){
+                par++;
+            }else{
+                imp++;
+            }
+
+            n/=10;
+        }
+
+        System.out.printf("El %d contiene %d dígitos pares y %d digitos impares.",guarda,par,imp);
+        
+    }
+}
